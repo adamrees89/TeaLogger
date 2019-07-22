4948b2a9b5ee0ca3f8153c6dab4015b5bd74ce1c
Adam Rees <adamrees89@users.noreply.github.com>
2019-07-22 07:58:52 +0000

    Update Branch (#12)
    * Add files via upload (#7)
    
    * Add files via upload
    
    * Add files via upload
    
    * Update to fix Week Counts (#8)
    
    * Adamrees89 patch 1 (#9)
    
    * Change Config
    
    * Delete config.ini
    
    * Add files via upload
    
    * Added milkly goodness (#10)
    
    * Added milkly goodness
    
    * Add milky goodness
    
    * Refactoring print function (#11)
    
    * Refactoring print function
    
    * Updated based on Hound comments

--- tealogger.py
+++ tealogger.py
@@ -10,15 +10,40 @@ Purpose of the script:
 
 # Import modules
 import datetime
+import time
+import sys
 import argparse
 import os
 import sqlite3
+import configparser
+
+"""
+Set up configuration file.
+"""
+path = "BrewConfig.ini"
+
+
+def createConfig(path):
+    txt = input("Enter the proposed location of the teabase.db file: ")
+    config = configparser.ConfigParser()
+    config['DIRECTORIES'] = {'workingDir': txt}
+    config['DIMENSIONS'] = {'diameter': 0.075,
+                            'depth': 0.085}
+    config['VOLUMES'] = {'milkVolume': 10}
+
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+    print("Updated Configuration, closing")
+    time.sleep(1)
+    sys.exit(0)
 
 """
 Set up and call argparse the later FunctionMap relates the modeChoices
 list to the Functions defined within this script
 """
-modeChoices = ["Cuppa", "24Hr", "Today", "Week", "Year", "Counts"]
+modeChoices = ["Cuppa", "24Hr", "Today", "Week",
+               "Year", "Counts", "Config"]
 parser = argparse.ArgumentParser()
 parser.add_argument("mode",
                     help="Which mode?",
@@ -29,8 +54,14 @@ parser.add_argument("-t", "--t", "-tea",
 parser.parse_args()
 args = parser.parse_args()
 
+if not os.path.exists(path):
+    createConfig(path)
+
+config = configparser.ConfigParser()
+config.read(path)
+workingDir = config.get("DIRECTORIES", "workingDir")
+
 # SQL Database Information
-workingDir = 'S:\Adam Rees'
 database = os.path.join(workingDir,'Teabase.db')
 
 # Function to connect to database
@@ -46,7 +77,7 @@ def ConnectDatabase(database):
         c.execute(table_sql)
     except:
         pass
-        
+
     return conn, c
 
 
@@ -57,19 +88,33 @@ def CloseCommitDatabase(conn):
 
 
 # Function to call another function
-def Decision(mode):
+def Decision(mode, path):
     FunctionMap = {"Cuppa": AddTea,
                    "24Hr": TwentyFourTotal,
                    "Today": TodayTotal,
                    "Week": WeekTotal,
                    "Year": AnnualTotal,
-                   "Counts": RunTotals}
-    FunctionMap[mode]()
+                   "Counts": RunTotals,
+                   "Config": createConfig}
+    func = FunctionMap[mode]
+
+    if mode == "Config":
+        func(path)
+    elif mode == "Cuppa":
+        func(args.t)
+    else:
+        func()
+
+
+# Function to make the printing to command line more uniform and pythonic
+def printInfo(count, period):
+    Milk = MilkAmount(count)
+    print(f"You have drunk {count} cups in the last {period}! ({Milk}ml of "
+          "Semi-Skimmed Milk)")
 
 
 # Function to add a cup to the sqlite3 file
-def AddTea():
-    count = args.t
+def AddTea(count):
     conn, c = ConnectDatabase(database)
     insert_sql = f"""INSERT INTO Tea
                 (createTime, beverage, count) VALUES (?,?,?)
@@ -90,6 +135,13 @@ def SQLCounting(selectsql):
     return TeaTotal
 
 
+# Function to calculate milk volume over a number of cups
+def MilkAmount(cups):
+    Milk = int(config.get("VOLUMES", "milkVolume")) * int(cups)
+
+    return Milk
+
+
 # Function to display today's running total
 def TwentyFourTotal():
     select_sql = """
@@ -97,7 +149,7 @@ def TwentyFourTotal():
                 where createTime > date('now', '-1 day')
                 """
     TeaTotal = SQLCounting(select_sql)
-    print(f"You have drunk {TeaTotal} cups in the last 24 hours!")
+    printInfo(TeaTotal, "24 Hours")
 
 
 def TodayTotal():
@@ -109,29 +161,37 @@ def TodayTotal():
                  where createTime > datetime({epoch},'unixepoch')
                  """
     TeaTotal = SQLCounting(select_sql)
-    print(f"You have drunk {TeaTotal} cups today!")
+    printInfo(TeaTotal, "today")
 
 
 # Function to display this weeks running total
 def WeekTotal():
-    select_sql = """
+    d = datetime.datetime.now()
+    lastMonday = d - datetime.timedelta(days=d.weekday())
+    midnight = datetime.datetime(d.year, d.month, lastMonday.day, 1, 0, 0, 0)
+    epoch = midnight.timestamp()
+    select_sql = f"""
                  SELECT SUM(count) from Tea
-                 where createTime > date('now', '-7 days')
+                 where createTime > datetime({epoch},'unixepoch')
                  """
     TeaTotal = SQLCounting(select_sql)
-    print(f"You have drunk {TeaTotal} cups this week!")
+    printInfo(TeaTotal, "week")
 
 
 # Function to display this years running total
 def AnnualTotal():
-    select_sql = """
+    d = datetime.datetime.now()
+    midnight = datetime.datetime(d.year, 1, 1, 1, 0, 0, 0)
+    epoch = midnight.timestamp()
+    select_sql = f"""
                  SELECT SUM(count) from Tea
-                 where createTime > date('now', '-1 year')
+                 where createTime > datetime({epoch},'unixepoch')
                  """
     TeaTotal = SQLCounting(select_sql)
-    print(f"You have drunk {TeaTotal} cups this year!")
+    printInfo(TeaTotal, "year")
 
 
+# Function to run the other functions
 def RunTotals():
     TodayTotal()
     TwentyFourTotal()
@@ -141,4 +201,4 @@ def RunTotals():
 
 # Check the script name and run the functions as required
 if __name__ == "__main__":
-    Decision(args.mode)
+        Decision(args.mode, path)
